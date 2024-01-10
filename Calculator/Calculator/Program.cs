0918ca04d588a1a16b4916a3ad7f479bc7281603
tomcatprime <karolchru@gmail.com>
2024-01-10 21:36:08 +0200

    Add project files.

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number: ");
+var userInput = Console.ReadLine();
+int firstNumber = int.Parse(userInput);
+Console.WriteLine("Input the second number: ");
+var userInput2 = Console.ReadLine();
+int secondNumber = int.Parse(userInput2);
+
+Console.WriteLine("What do you want to do with those numbers? ");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubstract");
+Console.WriteLine("[M]ultiply]");
+var whatUserSelect = Console.ReadLine();
+
+if (whatUserSelect == "A" || whatUserSelect == "a")
+{
+    var sum = firstNumber + secondNumber;
+    PrintFinalEquation(firstNumber, secondNumber, sum, "+");
+}
+else if (whatUserSelect == "S" || whatUserSelect == "s")
+{
+    var sum = firstNumber - secondNumber;
+    PrintFinalEquation(firstNumber, secondNumber, sum, "-");
+}
+else if (whatUserSelect == "M" || whatUserSelect == "m")
+{
+    var sum = firstNumber * secondNumber;
+    PrintFinalEquation(firstNumber, secondNumber, sum, "*");
+}
+else
+{
+    Console.WriteLine("Invalid Choise");
+}
+
+void PrintFinalEquation(int number1, int number2, int result, string @operator)
+{
+     Console.WriteLine(number1 + " " + @operator + " " + number2 + " = " + result);
+}
+Console.Write("Press any key to close");
+Console.Read();
+
+
---
+++ Todo-App/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("What do you want to do ?");
+Console.WriteLine("[S]ee all Todos");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+var userChoice = Console.ReadLine();
+
+// int.Parse
+
+if (userChoice == "S")
+{
+    PrintSelectedOption("See all TODOs");
+}
