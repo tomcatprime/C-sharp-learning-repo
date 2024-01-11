3889b8e22250def765ff71089c1b071f601e2678
tomcatprime <karolchru@gmail.com>
2024-01-11 17:59:24 +0200

    void and while loop

---
+++ Exercises C#/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+//loops while / do..while / for / foreach
+var userNumber = Console.ReadLine();
+var number = int.Parse(userNumber);
+while(number < 10)
+{
+
+    //number = number + 1; or
+    //number += 1; or
+    number++; //or ++number
+    Console.WriteLine($"Current number is {number}");
+}
+
+Console.WriteLine("Enter a word");
+var userInput = Console.ReadLine();
+while(userInput.Length < 15)
+{
+    userInput += 'a';
+    Console.WriteLine(userInput);
+}
+
+
+
+
+char ConvertPointtoGrade(int points)
+{
+    switch (points)
+    {
+        case 10:
+        case 9:
+            return 'A';
+        case 8:
+        case 7:
+        case 6:
+            return 'B';
+        default:
+            return '!';
+
+    }
+}
+
+
+Console.ReadLine();
+
+
+
+PrintNameFamilyname("karol", "szejk", 1, 10);
+
+void PrintNameFamilyname(string name, string familyname, int number, int number2)
+{
+    int currentNumber = number;
+    int sum = 0;
+    Console.WriteLine($"Your starting number is {currentNumber} and your starting sum is {sum}");
+    while (currentNumber < number2 )
+    {
+        sum += currentNumber;
+        currentNumber++;
+        Console.WriteLine($"You name is {name} and family name is {familyname}. The current number is {currentNumber} and sum is {sum}");
+    }
+}
+
+Console.ReadKey();
--- Todo-App/Program.cs
+++ Todo-App/Program.cs
@@ -9,9 +9,77 @@ Console.WriteLine("[E]xit");
 
 var userChoice = Console.ReadLine();
 
+
+switch(userChoice)
+{
+    case "S":
+    case "s":
+        PrintSelectedOption("See all TODOs");
+        break;
+    case "A":
+    case "a":
+        PrintSelectedOption("Add a TODO");
+        break;
+    case "R":
+    case "r":
+        PrintSelectedOption("Remove a TODOs");
+        break;
+    case "E":
+    case "e":
+        PrintSelectedOption("EXIT");
+        break;
+    default:
+        Console.WriteLine("Invalid Choise");
+        break;
+}
+
 // int.Parse
 
-if (userChoice == "S")
+
+
+void PrintSelectedOption(string selectedOption)
 {
-    PrintSelectedOption("See all TODOs");
+    Console.Write($"Selected option: {selectedOption}");
+}
+
+Console.ReadLine();
+//string interpolation - $"here is text and variable in {variable} text."
+int a = 4, b = 2, c = 10;
+Console.WriteLine($"First is: {a}, second is: {b}, third is {c}.");
+
+
+//switch case
+//if we have just single character, we can use char instead of string
+var number = 0;
+while (number < 10)
+{
+    number = number + 1;
+    Console.WriteLine($"Current number is {number}");
+}
+
+Console.WriteLine("The loop is finished");
+
+
+
+
+
+
+///////////////////////////////////////
+var dis = ConvertPointtoGrade(9);
+Console.WriteLine(dis);
+char ConvertPointtoGrade(int points)
+{
+    switch(points)
+    {
+        case 10:
+        case 9:
+            return 'A';
+        case 8:
+        case 7:
+        case 6:
+            return 'B';
+        default:
+            return '!';
+
+    }
 }
