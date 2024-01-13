f94f62f49b2ded7a12c4699f43053a4501ba1ffa
tomcatprime <karolchru@gmail.com>
2024-01-13 12:01:56 +0200

    do..while loop and for loop

---
+++ do-while-for loop/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//while..do loop
+string word;
+
+do
+{
+    Console.WriteLine("Enter a word longer thne 10 leeters");
+    word = Console.ReadLine();
+}
+while (word.Length <= 10);
+
+
+Console.WriteLine("The loop is finished");
+Console.ReadKey();
+
+//for loop
+for(int i= 1; i < 5; i+=2)
+{
+    Console.WriteLine("Loop run" + i);
+}
