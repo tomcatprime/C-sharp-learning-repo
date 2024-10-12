b98e281ddf153e4e1591494c2356a37cfbd33c8d
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-10-12 10:45:23 +0300

    commit

--- TryParse/TryParse/Program.cs
+++ TryParse/TryParse/Program.cs
@@ -1,15 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+Random random = new Random();
+//this will give us a random number between  1 and 10
+int randomNumber = random.Next(1, 11);
+
 Console.WriteLine("Give me a number");
 string inputString = Console.ReadLine();
 
 int num1;
+//creating an instance of the Random Class
+
+
 
 bool isNumber = int.TryParse(inputString, out num1);
 
 if(isNumber)
 {
+    if(num1 == randomNumber)
+    {
+        Console.WriteLine("You guessed the number");
+    }
+    else
+    {
+        Console.WriteLine("You didn't guessed the number");
+    }
     Console.WriteLine("Well done, you enetered a number");
 }
 else
@@ -19,6 +34,6 @@ else
 
 num1++;
 
-Console.WriteLine("User entered number +1" + num1);
+Console.WriteLine("User entered number +1 " + num1);
 Console.ReadKey();
 
