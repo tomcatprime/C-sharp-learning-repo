6da95c8ad4615fea6fa84abf76d4ff3ce454aa91
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-10-12 17:39:09 +0300

    Guess the number game

---
+++ GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome in guess the number game");
+
+Random random = new Random();
+
+int randomNumber = random.Next(1, 25);
+int counter = 0;
+int userGuess = 0;
+
+
+Console.WriteLine("Think of a number between 1 and 25 and type it.");
+
+while (userGuess != randomNumber)
+{
+    counter++;
+    userGuess = int.Parse(Console.ReadLine());
+    if (userGuess > randomNumber)
+    {
+        Console.WriteLine("Too high. Try Again");
+    }
+    else if (userGuess < randomNumber)
+    {
+        Console.WriteLine("Too low. Try Again");
+    }
+    else
+    {
+        Console.WriteLine("You guessed correctly in " + counter + " attempts");
+    }
+}
+
+Console.ReadKey();
---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int counter = 0;
+
+while (counter < 10)
+{
+    Console.WriteLine(counter);
+    counter++;
+}
+
+Console.ReadKey();
+
+//
+
+bool isGood = true;
+
+while (isGood)
+{
+    Console.WriteLine("Running....");
+    
+    isGood = false;
+}
+Console.WriteLine("Take a break");
+
+// guessing number game 
+
+Console.WriteLine("Welcome in Guess the number game");
+Random randomNum = new Random();
+
+int randomNumber = randomNum.Next(1, 10);
+
+bool isCorrectNumber = true;
+
+while (isCorrectNumber)
+{
+    Console.WriteLine("Guess a number between 1 and 10");
+    string userGuess = Console.ReadLine();
+    if (int.Parse(userGuess) == randomNumber)
+    {
+        Console.WriteLine("You guessed the number correctly");
+        isCorrectNumber = false;
+    }
+    else
+    {
+        Console.WriteLine("Keep trying, Enter a number.....");
+    }
+}
+
+Console.ReadLine();
