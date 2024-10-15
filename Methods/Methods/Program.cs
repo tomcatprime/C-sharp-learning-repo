3ca5309f1f2be9e79d65b21e2afa7e0efb56b8fb
tomcatprime <karolchru@gmail.com>
2024-10-15 21:36:10 +0300

    methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//method Void, return nothing, without parameter
+void WriteSomething()
+{
+    Console.WriteLine("I'm writing something");
+}
+
+WriteSomething();
+//method with parameter myString of type String
+//method declaration
+void WriteSomethingElse(string myString)
+{
+    Console.WriteLine("My string is " + myString);
+}
+//calling methond
+WriteSomethingElse("Something else");
+
+//Calling method using an Argument called mUsername
+
+string myUsername = "Frank";
+WriteSomethingElse(myUsername);
+
+//Method with return
+
+void AddTwoValues(int value1, int value2)
+{
+    int result = value1 + value2;
+    Console.WriteLine($"The result is {result}");
+}
+
+AddTwoValues(5,10);
