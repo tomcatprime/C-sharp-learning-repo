4350d8941b6675e30ec1db6ee2ac4c2edc7f810d
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-10-16 14:18:46 +0300

    Methods with parameters

--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -2,6 +2,8 @@
 
 
 //method Void, return nothing, without parameter
+using System.ComponentModel.DataAnnotations;
+
 void WriteSomething()
 {
     Console.WriteLine("I'm writing something");
@@ -23,11 +25,41 @@ string myUsername = "Frank";
 WriteSomethingElse(myUsername);
 
 //Method with return
-
-void AddTwoValues(int value1, int value2)
+//return method - instead of void we need to specify data type
+int AddTwoValues(int value1, int value2)
 {
     int result = value1 + value2;
-    Console.WriteLine($"The result is {result}");
+    return result;
 }
 
-AddTwoValues(5,10);
+
+int myResult = AddTwoValues(8, 16);
+Console.WriteLine("The result of AddTwoValues is: " + myResult);
+
+Console.WriteLine("Type a number");
+int num1 = int.Parse(Console.ReadLine());
+
+int myResult2 = AddTwoValues(num1, 10);
+Console.WriteLine($"The result of myResult2 is: " + myResult2);
+
+
+int SubstractTwoValues(int value1, int value2)
+{
+    int result = value1 - value2;
+    return result;
+}
+
+Console.WriteLine("Substraction:");
+Console.WriteLine("Type number 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Type number 2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+int myResult3 = SubstractTwoValues(number1, number2);
+
+Console.WriteLine("The result of substraction is " + myResult3);
+
+Console.WriteLine("Press any key to close the program....");
+
+
+Console.ReadKey();
