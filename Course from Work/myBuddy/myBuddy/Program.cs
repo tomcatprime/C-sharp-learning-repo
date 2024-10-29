805fb2ea0fca65f8cb77638976c6e9ac0ff60c04
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-10-29 21:05:41 +0200

    my buddy program

---
+++ Course from Work/myBuddy/myBuddy/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace myBuddy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in My Buddy program");
+
+            Trainee trainee = new Trainee();
+
+
+            trainee.dateOfBirth = DateTime.Now;
+
+
+
+            Console.WriteLine(trainee.GetAllDetails());
+        }
+    }
+}
---
+++ Course from Work/myBuddy/myBuddy/Trainee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myBuddy
+{
+    internal class Trainee
+    {
+
+        public int pcNumber;
+        public string traineeName;
+        public string phoneNumber;
+        public string emailId;
+        public string traineeGender;
+        public DateTime dateOfBirth;
+
+        public string GetAllDetails()
+        {
+            return $"PC Number: {pcNumber} \nTrainee Name: {traineeName} \nPhone Number: {phoneNumber} " +
+                $"\nTrainee Gender: {traineeGender} \nEmail-Id: {emailId} \nDate of Birth: {dateOfBirth}";
+        }
+
+
+
+    }
+}
